c3462ed5ada82d78577b37da97689abf6694e28c
Iangyl <vohanchib@gmail.com>
2020-02-04 00:05:18 +0200

    to do list design created

--- src/App.js
+++ src/App.js
@@ -6,14 +6,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import uuid from "uuid";
 
 class App extends Component {
+  state = {
+    items: [],
+    id: 0,
+    item: '',
+    editItem: false
+  };
+  handleChange = (e) => {
+    this.setState({item: e.target.value});
+  }
   render(){
     return(
       <div className='container'>
         <div className='row'>
           <div className='col-10 mx-auto col-md-8 mt-4'>
             <h3 className='text-capitalize text-center'>to do input</h3>
-            <ToDoInput />
-            <h3 className='text-capitalize text-center'>to do list</h3>
+            <ToDoInput item={this.state.item} handleChange={this.handleChange} />
             <ToDoList />
           </div>
         </div>
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -4,10 +4,11 @@ import ToDoItem from './ToDoItem';
 class ToDoList extends Component {
     render() {
         return (
-            <div>
-                Hello from ToDoList!
+            <ul className='list-group my-5'>
+                <h3 className='text-capitalize text-center'>To Do List</h3>
                 <ToDoItem />
-            </div>
+                <button type='button' className='btn btn-danger btn-block text-capitalize mt-5'>clear list</button>
+            </ul>
         );
     }
 }
